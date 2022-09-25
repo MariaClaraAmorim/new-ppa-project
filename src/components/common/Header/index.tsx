b2e8236b6e198ca038a3d5d355f34d47c9483b76
maria.amorim <mariaclaraamorim221@gmail.com>
2022-09-25 14:57:38 -0300

    feat: adiciona logo do sistema

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -5,22 +5,35 @@ import css from "./styles.module.css";
 function Header() {
   return (
     <div className={css.header}>
-      <AiOutlineUser className={css.iconUser} />
-      <p>Olá,</p>
-      <p>Usuário</p>
+      <div>
+        {/* <img src="public\LogoHeader.png" alt="" /> */}
 
-      <svg width="0" height="0">
-        <linearGradient id="blue-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
-          <stop stopColor="#7a6ded" offset="0%" />
-          <stop stopColor="#591885" offset="100%" />
-        </linearGradient>
-      </svg>
-      <a href="/carrinho">
-        <AiOutlineShoppingCart
-          className={css.iconCar}
-          style={{ fill: "url(#blue-gradient)" }}
-        />
-      </a>
+        <img className={css.imgHeader} src="\public\LogoHeader.png" />
+      </div>
+      <div className={css.details}>
+        <AiOutlineUser className={css.iconUser} />
+        <p>Olá,</p>
+        <p>Usuário</p>
+
+        <svg width="0" height="0">
+          <linearGradient
+            id="blue-gradient"
+            x1="100%"
+            y1="100%"
+            x2="0%"
+            y2="0%"
+          >
+            <stop stopColor="#7a6ded" offset="0%" />
+            <stop stopColor="#591885" offset="100%" />
+          </linearGradient>
+        </svg>
+        <a href="/carrinho">
+          <AiOutlineShoppingCart
+            className={css.iconCar}
+            style={{ fill: "url(#blue-gradient)" }}
+          />
+        </a>
+      </div>
     </div>
   );
 }
