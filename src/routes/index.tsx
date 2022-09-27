d344fa1ad47ebca07e42e4bae5d2824785e37515
maria.amorim <mariaclaraamorim221@gmail.com>
2022-09-27 20:42:02 -0300

    feat: adiciona novas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { MainTemplate } from "../layout/MainTemplate";
 import { MainTemplateLogin } from "../layout/MainTemplateLogin";
+import { CadProduto } from "../pages/Admin/CadProduto";
+import { AdminHome } from "../pages/Admin/Home";
 import { Cadastro } from "../pages/Cadastro";
+import { Carrinho } from "../pages/Carrinho";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Resultados } from "../pages/Resultados";
@@ -43,6 +46,33 @@ function RoutesApp() {
         element={
           <MainTemplate>
             <Resultados />
+          </MainTemplate>
+        }
+      />
+
+      <Route
+        path="/carrinho"
+        element={
+          <MainTemplate>
+            <Carrinho />
+          </MainTemplate>
+        }
+      />
+
+      <Route
+        path="/admin/home"
+        element={
+          <MainTemplate>
+            <AdminHome />
+          </MainTemplate>
+        }
+      />
+
+      <Route
+        path="/admin/cadastrar-produto"
+        element={
+          <MainTemplate>
+            <CadProduto />
           </MainTemplate>
         }
       />
