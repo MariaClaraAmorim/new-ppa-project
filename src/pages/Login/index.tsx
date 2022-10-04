6928a6c87cbf6c73f7b1159c054f045cf607a64b
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-10-04 18:23:39 -0300

    feat: Adiciona função de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,11 +2,46 @@ import { Button, Input } from "@chakra-ui/react";
 import { AiOutlineEyeInvisible } from "react-icons/ai";
 import css from "./styles.module.css";
 
+import { useNavigate } from "react-router-dom";
+import { FormEvent } from "react";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const send = async () => {
+    const data = {
+      username: "Maria",
+      password: "maria123",
+    };
+
+    const baseUrl = import.meta.env.VITE_API_URL;
+
+    const response = await fetch(`${baseUrl}/auth/sign-in`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) return alert("Oops!");
+
+    const json = await response.json();
+
+    alert(json.message ?? "Sucesso!");
+
+    navigate("/");
+  };
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await send();
+  };
+
   return (
     <>
       <div className={css.content}>
-        <form action="" className={css.form}>
+        <form action="/" onSubmit={onSubmit} className={css.form}>
           <ul className={css.title}>
             <a href="/login">
               <li>Login</li>
@@ -29,9 +64,9 @@ function Login() {
             <i></i>
           </div>
           <div className={css.teste}>
-            <a href="/">
-              <Button className={css.btn}>Entrar</Button>
-            </a>
+            <Button type="submit" className={css.btn}>
+              Entrar
+            </Button>
             <p>Esqueci minha senha</p>
           </div>
         </form>
